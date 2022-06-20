9b3b2ce7a5fc8a7c00c3df075d0c3585f8f923f6
Vahan78 <shield78@yandex.ru>
2022-06-20 22:30:14 +0400

    first commit in DigiSchool

---
+++ src/Lesson1.java
@@ -0,0 +1,15 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        char c = 33;
+        char d = '\u0021' ;
+        System.out.println(c + " = " + d);
+        int a = 12;
+        long b = 6546546l;
+        char cc = 65;
+        String name = "Vigen";
+        System.out.println(a + " " + b + " " + cc + " " + name);
+    }
+}
+
+
