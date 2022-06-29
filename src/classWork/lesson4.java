2878fe97b9640a4ba9b5eee93b4e6870d040e24e
Vahan78 <shield78@yandex.ru>
2022-06-29 12:23:49 +0400

    added ternar operation

--- src/classWork/lesson4.java
+++ src/classWork/lesson4.java
@@ -2,13 +2,13 @@ package classWork;
 
 public class lesson4 {
     public static void main(String[] args) {
-        int[] array = {10, 15,  0, 15, 10};
+        int[] array = {10, 7, 7, 15, 2, 15, 10};
         int max = array[0];
         int min = array[0];
         int indexMin = 0;
         int indexMax = 0;
         int countZero = 0;
-        int y=array[0];
+        int y = array[0];
 
         for (int i = 1; i < array.length; i++) {
             if (array[i] >= max) {
@@ -23,13 +23,13 @@ public class lesson4 {
             if (array[i] == 0) {
                 countZero++;
             }
-            
-
+            y = y ^ array[i];
 
 
         }
         System.out.println("Maximum number is " + max + " it index is " + indexMax);
         System.out.println("Minimum number is " + min + " it index is " + indexMin);
         System.out.println("Count of zerro in arrray is " + countZero);
+        System.out.println(y);
     }
 }
---
+++ src/classWork/simple.java
@@ -0,0 +1,11 @@
+package classWork;
+
+public class simple {
+    public static void main(String[] args) {
+        int a=4, b =3, c=5;
+        int y =a^b;
+        int x= y^a;
+        System.out.println(y);
+        System.out.println(x);
+    }
+}
